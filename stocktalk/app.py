aedf6958d2bbd8f064e9569b602f74f755e24d6d
Timothy Li <timothyli@college.harvard.edu>
2018-12-07 00:29:12 -0500

    fuck

--- stocktalk/app.py
+++ stocktalk/app.py
@@ -2,6 +2,7 @@ import os
 import psycopg2
 import matplotlib.pyplot as plt
 import numpy as np
+#import test
 from flask import Flask, render_template, g, request, jsonify, redirect, url_for, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -11,17 +12,23 @@ import json
 import requests
 from predict import *
 
+# postgresql-spherical-72286
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 db = create_engine('postgres://arbizklxkkfsjo:742246e607fcaaa7b1faf6e7dab54d082f551bd9abeeb3e51a4ef19dd3cca5bb@ec2-54-204-36-249.compute-1.amazonaws.com:5432/dcgq0vpeghnls2')
 
-# code used to create our users and stocks tables on PostgresSQL
 # db.execute("CREATE TABLE users (id int, username varchar(255), password varchar(255))")
 # db.execute("CREATE TABLE stocks (id int, username varchar(255), stock varchar(255))")
+# db = create_engine('postgres://arbizklxkkfsjo:742246e607fcaaa7b1faf6e7dab54d082f551bd9abeeb3e51a4ef19dd3cca5bb@ec2-54-204-36-249.compute-1.amazonaws.com:5432/dcgq0vpeghnls2')
 
-# via http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
-# Ensures user is logged-in in order to access particular pages
+# db.execute("CREATE TABLE stocks (id int, username varchar(255), ticker varchar(255))")
+
+# DATABASE_URL = os.environ['DATABASE_URL']
+# print(os.environ)
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+#via http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -33,7 +40,7 @@ def login_required(f):
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    # Ensure user has submitted an appropriate ticker
+	print(session.get("user_id"))
 	if request.method == "POST":
 		if not request.form.get("symbol"):
 			return redirect("/")
@@ -44,18 +51,15 @@ def index():
 			return render_template("base.html")
 		# global ticker
 		session["ticker"] = symbol
-        # API lookup based on ticker symbol - raw text saved as raw
 		url = 'https://api.iextrading.com/1.0/stock/' + symbol + '/chart/2y'
 		r = requests.get(url)
 		raw = json.loads(r.text)
 		data=[]
 		open_price=[]
 		date = []
-        # Iterate through each dictionary in raw, inserting dictionaries into a list of dictionaries.
 		for element in raw:
 			open_price.append(element["open"])
 			date.append(element["date"])
-        # Relevant in each dictionary includes date, open, volume, close, prediction, and ticker
 		data.append({"date":date[len(date)-1],"open":raw[len(raw)-1]["open"], "volume":raw[len(raw)-1]["volume"], "close":raw[len(raw)-1]["close"], "predict":run(symbol), "ticker":symbol.upper()})
 		return render_template("result.html", data=data)
 	return render_template("base.html")
@@ -63,18 +67,16 @@ def index():
 @app.route('/result', methods=['GET', 'POST'])
 @login_required
 def result():
-    # On POST submission, check if inputted ticker is already saved in user database
 	if request.method == "POST":
 		ticker = session["ticker"]
+		print(ticker)
 		try:
-            # If inputted ticker is already saved in the database, return user to bookmarks
 			rows = db.execute("SELECT * FROM stocks WHERE username = ('%s') AND stock = ('%s')" % (session.get("user_id"), ticker.upper()))
 			fetch = rows.fetchall()
 			if len(fetch) >= 1:
 				return redirect("bookmarks")
 		except:
 			pass
-        # If inputted ticker is not already saved in database, save the ticker by user_id
 		db.execute("INSERT INTO stocks (username, stock) VALUES ('%s', '%s')" % (session.get("user_id"), ticker.upper()))
 		return redirect("bookmarks")
 	return redirect("/")
@@ -83,7 +85,6 @@ def result():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     session.clear()
-    # Ensure that inputted username/password forms are filled
     if request.method == "POST":
         if not request.form.get("username"):
             return render_template("login.html")
@@ -91,24 +92,22 @@ def login():
         elif not request.form.get("password"):
             return render_template("login.html")
 
-        # Fetch information from database
         rows = db.execute("SELECT * FROM users WHERE username = '%s'" % (request.form.get("username")))
         fetch = rows.fetchall()
 
-        # If fetch does not return information, or if the passwords are incorrect, return user to login screen
         if not fetch or not check_password_hash(fetch[0]["password"], request.form.get("password")):
             return render_template("login.html")
 
-        # Assign user_id for each session
+        if len(fetch) == 0 or not check_password_hash(fetch[0]["password"], request.form.get("password")):
+            return render_template("login.html")
+
         session["user_id"] = request.form.get("username")
-        
-        # Direct user to predict page upon successful login
+        print(session.get("user_id"))
         return redirect("/")
     return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    # Ensure username, password, and confirmation forms are filled
     if request.method == "POST":
         if not request.form.get("username"):
             return render_template("register.html")
@@ -164,6 +163,7 @@ def bookmarks():
 
     bookmarks = db.execute("SELECT * FROM stocks WHERE username= '%s'" % session.get("user_id"))
     fetch = bookmarks.fetchall()
+    print(fetch)
     # fetch = [{"ticker":"aapl"},{"ticker":"amzn"}]
     data = []
     for stock in fetch:
@@ -175,12 +175,12 @@ def bookmarks():
         for element in raw:
             open_price.append(element["open"])
             date.append(element["date"])
+        #graph = graph.my_plotter(ax, open_price[len(open_price)-30:], date[len(open_price)-30:])
         data.append({"date":date[len(date)-1],"open":raw[len(raw)-1]["open"], "volume":raw[len(raw)-1]["volume"], "close":raw[len(raw)-1]["close"], "predict":run(stock["stock"]), "ticker":stock["stock"].upper()})
     return render_template("bookmarks.html", data=data)
 
 @app.route("/logout")
 @login_required
 def logout():
-    # Implementation of logout button
     session.clear()
     return redirect("/")
